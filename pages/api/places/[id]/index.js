bfaee3ae6d8d18a570e63952080c8d4c6d3617ad
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2024-09-20 16:27:36 +0200

    Update read data

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,29 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Place.js";
 
-export default function handler(request, response) {
+//import { db_comments } from "../../../../lib/db_comments";
+
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
+  if (request.method === "GET") {
+    const place = await Place.findById(id); //use singular
 
-  if (!id) {
-    return;
-  }
+    // if (!id) {
+    //   return;
+    // }
 
-  const place = db_places.find((place) => place._id.$oid === id);
+    /* const place = db_places.find((place) => place._id.$oid === id);
   const comment = place?.comments;
   const allCommentIds = comment?.map((comment) => comment.$oid) || [];
   const comments = db_comments.filter((comment) =>
     allCommentIds.includes(comment._id.$oid)
-  );
+  );*/
 
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
+    if (!place) {
+      return response.status(404).json({ status: "Not found" });
+    }
+
+    return response.status(200).json(place);
   }
-
-  response.status(200).json({ place: place, comments: comments });
 }
